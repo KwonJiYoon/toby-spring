2fe30aaa789fee01336118f7a554bc049d2f4cfa
jykwon <jykwon@12cm.co.kr>
2025-08-27 17:03:56 +0900

    feat: 1.2 DAO의 분리

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -4,7 +4,7 @@ import springbook.user.domain.User;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
         // DB 연결 기능
         Connection c = getConnection();
@@ -40,11 +40,24 @@ public class UserDao {
         return user;
     }
 
-    // 중복된 코드를 독립적인 메소드로 만들어서 중복 제거
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby_spring?serverTimezone=Asia/Seoul", "root", "yoon980112@");
+    // 추상 메서드로 변경 (구현 코드 제거) - 매서드 구현은 서브 클래스 담당
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 
-        return c;
+    public class NUserDao extends UserDao {
+        @Override
+        public Connection getConnection() throws ClassNotFoundException, SQLException {
+            // N사 DB Connection 생성 코드
+        }
     }
+
+    public class DUserDao extends UserDao {
+        @Override
+        public Connection getConnection() throws ClassNotFoundException, SQLException {
+            // D사 DB Connection 생성 코드
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
+        }
+    }
+
+
 }
