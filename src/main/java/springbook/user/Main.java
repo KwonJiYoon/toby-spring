f0fe5f689f3e6644a3b548595bd77831aba9ea83
jykwon <jykwon@12cm.co.kr>
2025-08-27 15:40:24 +0900

    feat: 1.1 초난감 DAO

--- src/main/java/springbook/user/Main.java
+++ src/main/java/springbook/user/Main.java
@@ -1,8 +1,30 @@
 package springbook.user;
 
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("yoon");
+        user.setName("지윤");
+        user.setPassword("Qwer1234!");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+
     }
 }
